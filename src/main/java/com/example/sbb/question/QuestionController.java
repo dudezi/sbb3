2f1bbfd2ea1ea43e609fbc85a9c25b1ea7acd3c8
dudezi <a01036952858@gmail.com>
2024-09-09 12:47:06 +0900

    refactor:form_error

--- src/main/java/com/example/sbb/question/QuestionController.java
+++ src/main/java/com/example/sbb/question/QuestionController.java
@@ -1,9 +1,15 @@
 package com.example.sbb.question;
 
+import com.example.sbb.answer.AnswerForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -22,21 +28,23 @@ public class QuestionController {
     }
 
     @GetMapping("/detail/{id}")
-    public String getQuestion (Model model, @PathVariable("id") Integer id) {
+    public String getQuestion (Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
     }
 
     @GetMapping("/create")
-    public String create() {
+    public String create(QuestionForm questionForm) {
         return "question_form";
     }
 
     @PostMapping("/create")
-    public String create(@RequestParam("subject") String subject, @RequestParam("content") String content) {
-        this.questionService.create(subject, content);
-
+    public String create(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list";
     }
 }
